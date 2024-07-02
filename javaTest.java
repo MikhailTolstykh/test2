df48acf276b7ec5e89cb933acbec8a49a8e8f77b
MikhailTolstykh <tolstich_m@mail.ru>
2024-07-02 16:05:53 +0700

    " commit mesage2"

--- javaTest.java
+++ javaTest.java
@@ -1,6 +1,11 @@
 public class javaTest {
     public static void main(String[] args) {
         javaTest javaTest = new javaTest();
+        try {
+            javaTest.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
